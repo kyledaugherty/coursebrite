a81568ed0206aef9643271fb3af6861edbb5e267
Kyle Daugherty <kyle.daugherty@gmail.com>
2016-02-21 23:28:50 -0600

    Install and configure pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  private
+
+  def user_not_authorized
+    redirect_to request.referrer || root_path,
+                status: :forbidden,
+                flash: { alert: "Access denied." }
+  end
 end
