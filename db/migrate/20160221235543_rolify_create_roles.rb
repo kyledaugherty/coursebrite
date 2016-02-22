2d1ad86846cc57834ca684feae3f3922b4230f45
Kyle Daugherty <kyle.daugherty@gmail.com>
2016-02-21 21:50:27 -0600

    Add constraints for Roles and UsersRoles

--- db/migrate/20160221235543_rolify_create_roles.rb
+++ db/migrate/20160221235543_rolify_create_roles.rb
@@ -1,19 +1,20 @@
 class RolifyCreateRoles < ActiveRecord::Migration
   def change
     create_table(:roles) do |t|
-      t.string :name
-      t.references :resource, :polymorphic => true
+      t.string :name, limit: 255, null: false
+      t.integer :resource_id, null: false
+      t.string :resource_type, limit: 255, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table(:users_roles, :id => false) do |t|
-      t.references :user
-      t.references :role
+      t.integer :user_id, null: false
+      t.integer :role_id, null: false
     end
 
     add_index(:roles, :name)
-    add_index(:roles, [ :name, :resource_type, :resource_id ])
-    add_index(:users_roles, [ :user_id, :role_id ])
+    add_index(:roles, [:name, :resource_type, :resource_id])
+    add_index(:users_roles, [:user_id, :role_id])
   end
 end
