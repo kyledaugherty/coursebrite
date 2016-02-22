421e28031bcb8311a36ac23e556274f7d6887219
Kyle Daugherty <kyle.daugherty@gmail.com>
2016-02-21 22:14:03 -0600

    Add validations for role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,9 +2,11 @@ class Role < ActiveRecord::Base
   belongs_to :resource, polymorphic: true
   has_and_belongs_to_many :users, join_table: :users_roles
 
+  validates :name, presence: true
+  validates :resource_id, presence: true
   validates :resource_type,
             inclusion: { in: Rolify.resource_types },
-            allow_nil: true
+            presence: true
 
   scopify
 end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,4 +3,11 @@ require "rails_helper"
 describe Role do
   it { should belong_to(:resource) }
   it { should have_and_belong_to_many(:users).join_table("users_roles") }
+  it do
+    should validate_inclusion_of(:resource_type).
+      in_array(Rolify.resource_types)
+  end
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:resource_id) }
+  it { should validate_presence_of(:resource_type) }
 end
